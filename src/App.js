5d6065ad12e6da776a4b0555c8957edf26932251
unknown <dollorinfotech@gmail.com>
2023-02-28 09:29:34 +0530

    html to reacted converted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Form, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Forms from './pages/Forms';
+import Layout from './components/ui/Layout';
 
+//2. Functional Componnent Defination area
 function App() {
+  //1. Hooks Area
+
+  //2. Function defination
+
+  //3. Return statement
+  //Every function return something  "to return html structure"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+           
+            <Route path="dashboard" element={ <Dashboard /> }></Route>
+            <Route path="forms" element={ <Forms /> }></Route>
+          </Route>
+          <Route index path="/" element={ <Login /> }></Route>
+          <Route path="/login" element={ <Login /> }></Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
